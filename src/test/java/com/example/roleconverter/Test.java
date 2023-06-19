9e7a8a8c8bcab233ada30b1eed9ff2f3ccf39feb
Java-couchpotato <64412019+Java-couchpotato@users.noreply.github.com>
2023-06-19 13:35:02 +0200

    test12

--- src/test/java/com/example/roleconverter/Test.java
+++ src/test/java/com/example/roleconverter/Test.java
@@ -1,10 +1,15 @@
 package com.example.roleconverter;
 
 public class Test {
-    public void test2(){
+    public void test2() {
 
     }
-      public void test22(){
+
+    public void test12() {
+
+    }
+
+    public void test22() {
 
     }
 }
